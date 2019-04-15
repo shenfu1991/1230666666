495f53f90e15fa1353d8c787838e6a0656363d01
caoxusheng <955082+caoxusheng@users.noreply.gitee.com>
2019-04-15 19:56:07 +0800

    邮箱为空的时候不发送

--- src/main/java/com/easyticket/notice/EmailNotice.java
+++ src/main/java/com/easyticket/notice/EmailNotice.java
@@ -3,6 +3,8 @@ package com.easyticket.notice;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+
 import com.jfinal.kit.LogKit;
 import com.jfinal.kit.PropKit;
 import com.jfplugin.mail.MailKit;
@@ -10,6 +12,10 @@ import com.jfplugin.mail.MailKit;
 public class EmailNotice {
 
 	public void run(String content) {
+		if (StringUtils.isBlank(PropKit.get("email.notice.mall")) || StringUtils.isBlank(PropKit.get("username"))
+				|| StringUtils.isBlank(PropKit.get("password"))) {
+			return;
+		}
 		List<String> list = new ArrayList<>();
 		list.add(PropKit.get("email.notice.mall"));
 		MailKit.send(PropKit.get("email.notice.mall"), list, "购票成功提醒", content);
